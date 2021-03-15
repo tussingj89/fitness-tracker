ed306ad8c6582ec39b4207c443a50d4520b1424d
tussingj89 <tussing40@gmail.com>
2021-03-14 20:42:45 -0400

    updated

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,6 +1,53 @@
-const db = require("../models");
+const db = require("../models")
 
-module.exports = (app) => {
+module.exports = function(app){ 
+    app.get("/api/workouts",function(req,res){  
+        db.Workout.find()
+        .then(data =>{  
+            res.json(data)
+        })
+        .catch(err => { 
+            res.json(err)
+        })
+    });
 
-    app.get("/api/")
+
+    app.post("/api/workouts",function (req,res){    
+        db.Workout.create({})
+        .then(data => res.json(data))
+        .catch(err => { 
+            res.json(err)
+        })
+    });
+
+    app.get("/api/workouts/range",function(req,res){  
+        db.Workout.find()
+        .then(data =>{  
+            res.json(data)
+        })
+        .catch(err => { 
+            res.json(err)
+        })
+    });
+
+
+    app.post("/api/workouts/range",function (req,res){    
+        db.Workout.create({})
+        .then(data => res.json(data))
+        .catch(err => { 
+            res.json(err)
+        })
+    });
+
+    app.put("/api/workouts/:id",({body,params},res)=>{   
+        db.Workout.findByIdAndUpdate(  
+         params.id,
+         {$push:{exercises:body} },
+         {new: true,runValidators:true }
+        )
+        .then(data => res.json(data))
+        .catch(err => { 
+            res.json(err)
+        })
+    });
 }
